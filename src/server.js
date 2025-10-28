c65e772b47b20710a953f1f190e25ec48b3d5d05
vasilijeIT23 <mucibabic.vasilije21@gmail.com>
2025-10-28 17:29:37 +0100

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
+import job from "'/config/cron.js";
 
 const app = express()
 const PORT = ENV || 5001
+
+if(ENV.NODE_ENV === "production") job.start()
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ success: true });
