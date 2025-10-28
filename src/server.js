7027ab89264c9b26d3395ad761f96faa1bfd4010
vasilijeIT23 <mucibabic.vasilije21@gmail.com>
2025-10-28 17:34:01 +0100

    cron.js added fix

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
-import job from "'/config/cron.js";
+import job from "./config/cron.js";
 
 const app = express()
 const PORT = ENV || 5001
